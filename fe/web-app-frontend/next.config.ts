f72a3e1a9c480724ec4da61570fa1741a8762432
phuocly789 <phuoclyminh789@gmail.com>
2026-06-21 18:11:59 +0700

    fix 401

--- fe/web-app-frontend/next.config.ts
+++ fe/web-app-frontend/next.config.ts
@@ -9,8 +9,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: "/api/sapo/:path*",
-        destination: `${BACKEND_URL}/api/sapo/:path*`,
+        source: "/api/:path*",
+        destination: `${BACKEND_URL}/api/:path*`,
       },
     ];
   },
